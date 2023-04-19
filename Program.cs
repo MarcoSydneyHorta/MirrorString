5acdf23ddbbef8b9fcfa10033e4f165309ed3d5e
MarcoSydneyHorta <marco.sh@outlook.com>
2023-04-19 14:48:38 -0300

    Verify valid characters

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MirrorString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the string");
+            Console.Write("Enter the string: ");
             string ent = Console.ReadLine();
             string mirr = Mirror(ent);
             Console.WriteLine("The mirror string is: " + mirr);
@@ -15,12 +15,34 @@ namespace MirrorString
         {
 
             int tam = ent.Length;
+            int ini = 0;
             string[] outp = new string[tam];
-            for (int i = 0; i < tam; i++)
+            int taal = tam; // if is not a valid string
+
+            if (!(ent[0] >= 'a' && ent[0] <= 'z' || ent[0] >= 'A' && ent[0] <= 'Z'))
+            {
+                outp[0] = ent[0].ToString();
+                ini = 1;
+                taal--;
+            }
+            if (!(ent[tam - 1] >= 'a' && ent[tam - 1] <= 'z' || ent[tam - 1] >= 'A' && ent[tam - 1] <= 'Z'))
+            {
+                outp[tam - 1] = ent[tam - 1].ToString();
+                taal--;
+            }
+
+            for (int i = ini; i < tam; i++)
             {
                 if (ent[i] >= 'a' && ent[i] <= 'z' || ent[i] >= 'A' && ent[i] <= 'Z')
                 {
-                    outp[i] = ent[tam - i - 1].ToString();  // Change to opposite position
+                    if (tam != taal)
+                    {
+                        outp[i] = ent[taal - i - 1].ToString();  // Change to opposite position                
+                    }
+                    else
+                    {
+                        outp[i] = ent[tam - i - 1].ToString();  // Change to opposite position
+                    }
                 }
                 else
                 {
